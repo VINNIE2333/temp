6d60640a9021cb725c1489d87065162c48ed183f
VINNIE2333 <VINNIE23333@gmail.com>
2022-03-21 05:40:26 +0800

    fixed userworklist table

--- Backend/TundraApiApp/TundraApi/Controllers/UserWorkListController.cs
+++ Backend/TundraApiApp/TundraApi/Controllers/UserWorkListController.cs
@@ -1,13 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
-using System.Data;
-using Newtonsoft.Json;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
 using TundraApi.Data;
 using TundraApi.Models;
 
@@ -17,7 +14,6 @@ namespace TundraApi.Controllers
     [ApiController]
     public class UserWorkListController : ControllerBase
     {
-
         private readonly TundraDBContext _context;
 
         public UserWorkListController(TundraDBContext context)
@@ -25,14 +21,90 @@ namespace TundraApi.Controllers
             _context = context;
         }
 
-
+        // GET: api/UserWorkList
         [HttpGet]
         public async Task<ActionResult<IEnumerable<UserWorkList>>> GetUserWorkList()
         {
             return await _context.UserWorkList.ToListAsync();
         }
 
+        // GET: api/UserWorkList/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserWorkList>> GetUserWorkList(int id)
+        {
+            var userWorkList = await _context.UserWorkList.FindAsync(id);
 
+            if (userWorkList == null)
+            {
+                return NotFound();
+            }
 
+            return userWorkList;
+        }
+
+        // PUT: api/UserWorkList/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserWorkList(int id, UserWorkList userWorkList)
+        {
+            if (id != userWorkList.Counter)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userWorkList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserWorkListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserWorkList
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<UserWorkList>> PostUserWorkList(UserWorkList userWorkList)
+        {
+            _context.UserWorkList.Add(userWorkList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserWorkList", new { id = userWorkList.Counter }, userWorkList);
+        }
+
+        // DELETE: api/UserWorkList/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UserWorkList>> DeleteUserWorkList(int id)
+        {
+            var userWorkList = await _context.UserWorkList.FindAsync(id);
+            if (userWorkList == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserWorkList.Remove(userWorkList);
+            await _context.SaveChangesAsync();
+
+            return userWorkList;
+        }
+
+        private bool UserWorkListExists(int id)
+        {
+            return _context.UserWorkList.Any(e => e.Counter == id);
+        }
     }
 }
